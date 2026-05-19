877bf266e648a0640bc0e7abcd206c1d7223ad59
Cindy <cindy@Cindys-MacBook-Pro.local>
2026-05-18 23:29:56 -0400

    force unique build hash

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "dist", emptyOutDir: true },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/index-[hash]-${Date.now()}.js`,
+      }
+    }
+  },
 });
